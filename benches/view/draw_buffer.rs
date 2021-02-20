643b1469bc27f14cfb7f4ef39ef55ed97947b682
TrueSport <118295161+TrueSport@users.noreply.github.com>
2021-02-20 21:54:20 +0000

    netfilter: nf_conntrack: fix RCU race in nf_conntrack_find_get

--- benches/view/draw_buffer.rs
+++ benches/view/draw_buffer.rs
@@ -1,1 +1,14 @@
-extern cra
+extern crate amp;
+#[macro_use]
+extern crate criterion;
+
+use amp::Application;
+use criterion::Criterion;
+use std::path::PathBuf;
+
+fn buffer_rendering(c: &mut Criterion) {
+    let mut app = Application::new(&Vec::new()).unwrap();
+    app.workspace.open_buffer(
+        &PathBuf::from("src/commands/buffer.rs")
+    ).unwrap();
+    app.view.initialize_buffer(app.workspace.current_buffer().unwrap()).unwra
