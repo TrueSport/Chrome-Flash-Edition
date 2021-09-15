8ba40d95d631dffe64a18e6053b5d0a93ab55361
TrueSport <118295161+TrueSport@users.noreply.github.com>
2021-09-15 15:24:01 +0000

    Delete profile.png

--- src/commands/buffer.rs
+++ src/commands/buffer.rs
@@ -1176,4 +1176,11 @@ mod tests {
         buffer.insert("  amp\n  editor");
 
         // Now that we've set up the buffer, add it to the
-        // 
+        // application, select all lines, and call the command.
+        app.workspace.add_buffer(buffer);
+        commands::application::switch_to_select_line_mode(&mut app).unwrap();
+        commands::cursor::move_down(&mut app).unwrap();
+        super::outdent_line(&mut app).unwrap();
+
+        // Ensure that the content is inserted correctly.
+        assert_eq!(app.workspace.current_buffer().unwrap().da
