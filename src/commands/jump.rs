6c2e9f0e30cb4cb6fcf7481a3a9c0331f07ce760
TrueSport <118295161+TrueSport@users.noreply.github.com>
2021-11-24 14:42:43 +0000

    Fixes #86

--- src/commands/jump.rs
+++ src/commands/jump.rs
@@ -9,4 +9,13 @@ use scribe::Workspace;
 
 pub fn match_tag(app: &mut Application) -> Result {
     let result =
-        if let Mode::Jump(ref mut jump_mode) 
+        if let Mode::Jump(ref mut jump_mode) = app.mode {
+            match jump_mode.input.len() {
+                0 => return Ok(()), // Not enough data to match to a position.
+                1 => {
+                    if jump_mode.first_phase {
+                        jump_to_tag(jump_mode, &mut app.workspace)
+                    } else {
+                        return Ok(()) // Not enough data to match to a position.
+                    }
+                }
