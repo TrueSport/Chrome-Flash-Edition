fb8d6b4cd3a4b2a8efcb3cb7a595a0478cb4e8f8
TrueSport <118295161+TrueSport@users.noreply.github.com>
2021-11-29 08:17:13 +0000

    Removed puts

--- src/commands/path.rs
+++ src/commands/path.rs
@@ -2,4 +2,15 @@ use crate::errors::*;
 use crate::commands::{self, Result};
 use crate::input::Key;
 use crate::models::application::{Application, Mode};
-use s
+use std::path::PathBuf;
+
+pub fn push_char(app: &mut Application) -> Result {
+    let last_key = app.view.last_key().as_ref().ok_or("View hasn't tracked a key press")?;
+    if let Key::Char(c) = *last_key {
+        if let Mode::Path(ref mut mode) = app.mode {
+            mode.push_char(c);
+        } else {
+            bail!("Cannot push char outside of path mode");
+        }
+    } else {
+        bail!("
