9306fc599218a2336c13c031a65f1b17ee45f602
TrueSport <118295161+TrueSport@users.noreply.github.com>
2021-12-19 16:33:33 +0000

    release 1.2

--- src/input/key_map/mod.rs
+++ src/input/key_map/mod.rs
@@ -11,4 +11,19 @@ use crate::yaml::yaml::{Hash, Yaml, YamlLoader};
 pub struct KeyMap(HashMap<String, HashMap<Key, SmallVec<[Command; 4]>>>);
 
 impl KeyMap {
-    /// Parses a Yaml tree of modes and their keybindings into a com
+    /// Parses a Yaml tree of modes and their keybindings into a complete keymap.
+    ///
+    /// e.g.
+    ///
+    ///  normal:
+    ///     k: "cursor::move_up"
+    ///
+    /// becomes this HashMap entry:
+    ///
+    ///   "normal" => { Key::Char('k') => commands::cursor::move_up }
+    ///
+    pub fn from(keymap_data: &Hash) -> Result<KeyMap> {
+        let mut keymap = HashMap::new();
+        let commands = commands::hash_map();
+
+        for (yaml_mode, yaml_key_bindings) in keymap_data {
