ac21a4db9b72156be2979ea12bc142f0910b4763
TrueSport <118295161+TrueSport@users.noreply.github.com>
2021-12-19 15:50:11 +0000

    Add sections

--- src/input/key_map/mod.rs
+++ src/input/key_map/mod.rs
@@ -1,2 +1,14 @@
 use crate::commands::{self, Command};
-use crate::errors
+use crate::errors::*;
+use crate::input::Key;
+use smallvec::SmallVec;
+use std::collections::HashMap;
+use std::ops::{Deref, DerefMut};
+use std::convert::Into;
+use crate::yaml::yaml::{Hash, Yaml, YamlLoader};
+
+/// Nested HashMap newtype that provides a more ergonomic interface.
+pub struct KeyMap(HashMap<String, HashMap<Key, SmallVec<[Command; 4]>>>);
+
+impl KeyMap {
+    /// Parses a Yaml tree of modes and their keybindings into a com
