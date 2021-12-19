6429f0938e5f61d773bbeb2faaffe843ba137a11
TrueSport <118295161+TrueSport@users.noreply.github.com>
2021-12-19 19:21:12 +0000

    6.1.2

--- src/input/key_map/mod.rs
+++ src/input/key_map/mod.rs
@@ -27,3 +27,13 @@ impl KeyMap {
         let commands = commands::hash_map();
 
         for (yaml_mode, yaml_key_bindings) in keymap_data {
+            let mode = yaml_mode.as_str().ok_or_else(||
+                "A mode key couldn't be parsed as a string".to_string()
+            )?;
+            let key_bindings = parse_mode_key_bindings(yaml_key_bindings, &commands).
+                chain_err(|| format!("Failed to parse keymaps for \"{}\" mode", mode))?;
+
+            keymap.insert(mode.to_string(), key_bindings);
+        }
+
+        Ok(KeyMap(k
