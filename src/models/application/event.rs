84da1c033ef746a956153c358b7bbcabfe41aa31
TrueSport <118295161+TrueSport@users.noreply.github.com>
2022-02-10 07:50:34 +0000

    Lots of fixes

--- src/models/application/event.rs
+++ src/models/application/event.rs
@@ -1,2 +1,9 @@
 use crate::input::Key;
-use crate::models::application
+use crate::models::application::modes::open::Index;
+
+#[derive(Debug, PartialEq)]
+pub enum Event {
+    Key(Key),
+    Resize,
+    OpenModeIndexComplete(Index)
+}
---
+++ src/models/application/mod.rs
@@ -0,0 +1,266 @@
+
+mod clipboard;
+mod event;
+pub mod modes;
+mod preferences;
+
+// Published API
+pub use self::clipboard::ClipboardContent;
+pub use self::event::Event;
+pub use self::preferences::Preferences;
+
+use self::clipboard::Clipboard;
+use self::modes::*;
+use crate::commands;
+use crate::errors::*;
+use git2::Repository;
+use crate::presenters;
+use scribe::{Buffer, Workspace};
+use std::cell::RefCell;
+use std::env;
+use std::path::Path;
+use std::rc::Rc;
+use std::sync::mpsc::{self, Receiver, Sender};
+use crate::view::View;
+
+pub enum Mode {
+    Confirm(ConfirmMode),
+    Command(CommandMode),
+    Exit,
+    Insert,
+    Jump(JumpMode),
+    LineJump(LineJumpMode),
+    Path(PathMode),
+    Normal,
+    Open(OpenMode),
+    Select(SelectMode),
+    SelectLine(SelectLineMode),
+    Search(SearchMode),
+    SymbolJump(SymbolJumpMode),
+    Syntax(SyntaxMode),
+    Theme(ThemeMode),
+}
+
+pub struct Application {
+    pub mode: Mode,
+    pub workspace: Workspace,
+    pub search_query: Option<String>,
+    pub view: View,
+    pub clipboard: Clipboard,
+    pub repository: Option<Repository>,
+    pub error: Option<Error>,
+    pub preferences: Rc<RefCell<Preferences>>,
+    pub event_channel: Sender<Event>,
+    events: Receiver<Event>,
+}
+
+impl Application {
+    pub fn new(args: &Vec<String>) -> Result<Application> {
+        let preferences = initialize_preferences();
+
+        let (event_channel, events) = mpsc::channel();
+        let mut view = View::new(preferences.clone(), event_channel.clone())?;
+        let clipboard = Clipboard::new();
+
+        // Set up a workspace in the current directory.
+        let workspace = create_workspace(&mut view, &preferences.borrow(), args)?;
+
+        Ok(Application {
+            mode: Mode::Normal,
+            workspace,
+            search_query: None,
+            view,
+            clipboard,
+            repository: Repository::discover(&env::current_dir()?).ok(),
+            error: None,
+            preferences,
+            event_channel,
+            events,
+        })
+    }
+
+    pub fn run(&mut self) -> Result<()> {
+        loop {
+            self.render();
+            self.wait_for_event()?;
+
+            if let Mode::Exit = self.mode {
+                break;
+            }
+        }
+
+        Ok(())
+    }
+
+    fn render(&mut self) {
+        if let Err(error) = self.present() {
+            presenters::error::display(&mut self.workspace, &mut self.view, &error);
+        } else if let Some(ref error) = self.error {
+            // Display an error from previous command invocation, if one exists.
+            presenters::error::display(&mut self.workspace, &mut self.view, error);
+        }
+    }
+
+    fn present(&mut self) -> Result<()> {
+        match self.mode {
+            Mode::Confirm(_) => {
+                presenters::modes::confirm::display(&mut self.workspace, &mut self.view)
+            }
+            Mode::Command(ref mut mode) => {
+                presenters::modes::search_select::display(&mut self.workspace, mode, &mut self.view)
+            }
+            Mode::Insert => presenters::modes::insert::display(&mut self.workspace, &mut self.view),
+            Mode::Open(ref mut mode) => {
+                presenters::modes::search_select::display(&mut self.workspace, mode, &mut self.view)
+            }
+            Mode::Search(ref mode) => {
+                presenters::modes::search::display(&mut self.workspace, mode, &mut self.view)
+            }
+            Mode::Jump(ref mut mode) => {
+                presenters::modes::jump::display(&mut self.workspace, mode, &mut self.view)
+            }
+            Mode::LineJump(ref mode) => {
+                presenters::modes::line_jump::display(&mut self.workspace, mode, &mut self.view)
+            }
+            Mode::Path(ref mode) => {
+                presenters::modes::path::display(&mut self.workspace, mode, &mut self.view)
+            }
+            Mode::SymbolJump(ref mut mode) => {
+                presenters::modes::search_select::display(&mut self.workspace, mode, &mut self.view)
+            }
+            Mode::Syntax(ref mut mode) => {
+                presenters::modes::search_select::display(&mut self.workspace, mode, &mut self.view)
+            }
+            Mode::Select(ref mode) => {
+                presenters::modes::select::display(&mut self.workspace, mode, &mut self.view)
+            }
+            Mode::SelectLine(ref mode) => {
+                presenters::modes::select_line::display(&mut self.workspace, mode, &mut self.view)
+            }
+            Mode::Normal => presenters::modes::normal::display(
+                &mut self.workspace,
+                &mut self.view,
+                &self.repository,
+            ),
+            Mode::Theme(ref mut mode) => {
+                presenters::modes::search_select::display(&mut self.workspace, mode, &mut self.view)
+            }
+            Mode::Exit => Ok(()),
+        }
+    }
+
+    fn wait_for_event(&mut self) -> Result<()> {
+        let event = self
+            .events
+            .recv()
+            .chain_err(|| "Error receiving application event")?;
+        match event {
+            Event::Key(key) => {
+                self.view.last_key = Some(key);
+                self.error = commands::application::handle_input(self).err();
+            }
+            Event::Resize => {}
+            Event::OpenModeIndexComplete(index) => {
+                if let Mode::Open(ref mut open_mode) = self.mode {
+                    open_mode.set_index(index);
+
+                    // Trigger a search, in case a query was
+                    // entered while we were indexing.
+                    open_mode.search();
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    pub fn mode_str(&self) -> Option<&'static str> {
+        match self.mode {
+            Mode::Command(ref mode) => if mode.insert_mode() {
+                Some("search_select_insert")
+            } else {
+                Some("search_select")
+            },
+            Mode::SymbolJump(ref mode) => if mode.insert_mode() {
+                Some("search_select_insert")
+            } else {
+                Some("search_select")
+            },
+            Mode::Open(ref mode) => if mode.insert_mode() {
+                Some("search_select_insert")
+            } else {
+                Some("search_select")
+            },
+            Mode::Theme(ref mode) => if mode.insert_mode() {
+                Some("search_select_insert")
+            } else {
+                Some("search_select")
+            },
+            Mode::Syntax(ref mode) => if mode.insert_mode() {
+                Some("search_select_insert")
+            } else {
+                Some("search_select")
+            },
+            Mode::Normal => Some("normal"),
+            Mode::Path(_) => Some("path"),
+            Mode::Confirm(_) => Some("confirm"),
+            Mode::Insert => Some("insert"),
+            Mode::Jump(_) => Some("jump"),
+            Mode::LineJump(_) => Some("line_jump"),
+            Mode::Select(_) => Some("select"),
+            Mode::SelectLine(_) => Some("select_line"),
+            Mode::Search(ref mode) => if mode.insert_mode() {
+                Some("search_insert")
+            } else {
+                Some("search")
+            },
+            Mode::Exit => None,
+        }
+    }
+}
+
+fn initialize_preferences() -> Rc<RefCell<Preferences>> {
+    Rc::new(RefCell::new(
+        Preferences::load().unwrap_or_else(|_| Preferences::new(None)),
+    ))
+}
+
+fn create_workspace(view: &mut View, preferences: &Preferences, args: &Vec<String>) -> Result<Workspace> {
+    // Discard the executable portion of the argument list.
+    let mut path_args = args.iter().skip(1).peekable();
+
+    // Move into an argument-specified directory, if present.
+    let initial_dir = env::current_dir()?;
+    if let Some(arg) = path_args.peek() {
+        let path = Path::new(&arg);
+
+        if path.is_dir() {
+            env::set_current_dir(path.canonicalize()?)?;
+        }
+    }
+
+    let workspace_dir = env::current_dir()?;
+    let mut workspace = Workspace::new(&workspace_dir)?;
+
+    // Load user syntax definitions.
+    //
+    // It's important to do this before opening buffers, as that's when syntax
+    // definitions are associated; we want the complete set before that happens.
+    let syntax_path = Preferences::syntax_path()?;
+    if let Err(e) = workspace.syntax_set.load_syntaxes(syntax_path, true) {
+        bail!("Failed to load user syntaxes: {:?}", e);
+    }
+    workspace.syntax_set.link_syntaxes();
+
+    // If the first argument was a directory, we've navigated into
+    // it; skip it before evaluating file args, lest we interpret
+    // it again as a non-existent file and create a buffer for it.
+    if workspace_dir != initial_dir { path_args.next(); }
+
+    // Try to open specified files.
+    for path_arg in path_args {
+        let path = Path::new(&path_arg);
+
+        if path.is_dir() { continue; }
+
+        // Check if the user has provided any syntax preference for this file.
