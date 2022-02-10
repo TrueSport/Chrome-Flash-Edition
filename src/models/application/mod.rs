7fb39cb1321ba3fc706657f8354d7dec86d95948
TrueSport <118295161+TrueSport@users.noreply.github.com>
2022-02-10 17:27:30 +0000

    Release v0.9.0

--- src/models/application/mod.rs
+++ src/models/application/mod.rs
@@ -264,3 +264,95 @@ fn create_workspace(view: &mut View, preferences: &Preferences, args: &Vec<Strin
         if path.is_dir() { continue; }
 
         // Check if the user has provided any syntax preference for this file.
+        // If not, a default one will be applied on calling workspace.add_buffer()
+        let syntax_definition =
+            preferences.syntax_definition_name(&path).and_then(|name| {
+                workspace.syntax_set.find_syntax_by_name(&name).cloned()
+            });
+
+        // Open the specified path if it exists, or
+        // create a new buffer pointing to it if it doesn't.
+        let argument_buffer = if path.exists() {
+            let mut buffer = Buffer::from_file(path)?;
+            buffer.syntax_definition = syntax_definition;
+
+            buffer
+        } else {
+            let mut buffer = Buffer::new();
+            buffer.syntax_definition = syntax_definition;
+
+            // Point the buffer to the path, ensuring that it's absolute.
+            if path.is_absolute() {
+                buffer.path = Some(path.to_path_buf());
+            } else {
+                buffer.path = Some(workspace.path.join(path));
+            }
+
+            buffer
+        };
+
+        workspace.add_buffer(argument_buffer);
+        view.initialize_buffer(workspace.current_buffer().unwrap())?;
+    }
+
+    Ok(workspace)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Application;
+    use crate::view::View;
+    use super::preferences::Preferences;
+
+    use yaml::YamlLoader;
+    use scribe::Buffer;
+    use std::cell::RefCell;
+    use std::env;
+    use std::path::Path;
+    use std::rc::Rc;
+    use std::sync::mpsc;
+
+    #[test]
+    fn application_uses_file_arguments_to_load_contents_into_buffers_when_files_exist() {
+        let mut application =
+            Application::new(&vec![String::new(), String::from("Cargo.lock")]).unwrap();
+        let buffer = Buffer::from_file(Path::new("Cargo.lock")).unwrap();
+
+        assert_eq!(
+            application.workspace.current_buffer().unwrap().path,
+            buffer.path
+        );
+        assert_eq!(
+            application.workspace.current_buffer().unwrap().data(),
+            buffer.data()
+        );
+    }
+
+    #[test]
+    fn application_uses_file_arguments_to_create_new_buffers_when_files_do_not_exist() {
+        let mut application =
+            Application::new(&vec![String::new(), String::from("non_existent_file")]).unwrap();
+
+        assert_eq!(
+            application.workspace.current_buffer().unwrap().path,
+            Some(env::current_dir().unwrap().join("non_existent_file"))
+        );
+        assert_eq!(application.workspace.current_buffer().unwrap().data(), "");
+    }
+
+    #[test]
+    fn create_workspace_correctly_applies_user_defined_syntax_when_opening_buffer_from_command_line() {
+        let data = YamlLoader::load_from_str("types:\n  xyz:\n    syntax: Rust").unwrap();
+        let preferences = Rc::new(RefCell::new(Preferences::new(data.into_iter().nth(0))));
+        let (event_channel, _) = mpsc::channel();
+        let mut view = View::new(preferences.clone(), event_channel.clone()).unwrap();
+
+        let args = vec![String::new(), String::from("src/test.xyz")];
+        let mut workspace = super::create_workspace(&mut view, &preferences.borrow(), &args).unwrap();
+
+        assert_eq!(
+            workspace.current_buffer().unwrap().syntax_definition.as_ref().unwrap().name,
+            "Rust"
+        );
+    }
+}
---
+++ src/models/application/modes/command/displayable_command.rs
@@ -0,0 +1,12 @@
+use std::fmt;
+use crate::commands::Command;
+
+// Utility type to make an Amp command function presentable (via the
+// Display trait), which is required for any type used in search/select mode.
+pub struct DisplayableCommand {
+    pub description: &'static str,
+    pub command: Command,
+}
+
+impl fmt::Display for DisplayableCommand {
+    fn fmt(
