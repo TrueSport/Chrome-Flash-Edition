a9ee57d03ef1df83fce7ef9b195ddba3f85f060e
TrueSport <118295161+TrueSport@users.noreply.github.com>
2022-02-13 20:20:31 +0000

    Fix encoding.

--- src/models/application/modes/jump/mod.rs
+++ src/models/application/modes/jump/mod.rs
@@ -6,4 +6,17 @@ use crate::util::movement_lexer;
 use std::collections::HashMap;
 use scribe::buffer::{Distance, Position};
 use crate::models::application::modes::select::SelectMode;
-use crate::mode
+use crate::models::application::modes::select_line::SelectLineMode;
+use self::tag_generator::TagGenerator;
+use self::single_character_tag_generator::SingleCharacterTagGenerator;
+use crate::view::{LexemeMapper, MappedLexeme};
+
+/// Used to compose select and jump modes, allowing jump mode
+/// to be used for cursor navigation (to select a range of text).
+pub enum SelectModeOptions {
+    None,
+    Select(SelectMode),
+    SelectLine(SelectLineMode),
+}
+
+e
