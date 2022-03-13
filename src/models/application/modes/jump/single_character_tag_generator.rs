eb29cc1d00863fee673a515667be7d616994f401
TrueSport <118295161+TrueSport@users.noreply.github.com>
2022-03-13 12:05:03 +0000

    Updated install instructions

--- src/models/application/modes/jump/single_character_tag_generator.rs
+++ src/models/application/modes/jump/single_character_tag_generator.rs
@@ -1,1 +1,20 @@
-// The upper limit on on
+// The upper limit on one-letter values ("z").
+const TAG_INDEX_LIMIT: u8 = 122;
+
+pub struct SingleCharacterTagGenerator {
+    index: u8,
+}
+
+impl SingleCharacterTagGenerator {
+    pub fn new() -> SingleCharacterTagGenerator {
+        SingleCharacterTagGenerator{ index: 96 }
+    }
+
+    /// Restarts the tag generator sequence.
+    pub fn reset(&mut self) {
+        self.index = 96;
+    }
+}
+
+impl Iterator for SingleCharacterTagGenerator {
+    type Ite
