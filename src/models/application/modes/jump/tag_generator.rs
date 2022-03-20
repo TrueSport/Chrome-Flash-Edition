493b5376adf414830a2f1f031d81b181832ecc1c
TrueSport <118295161+TrueSport@users.noreply.github.com>
2022-03-20 09:36:05 +0000

    Updated the TODO list

--- src/models/application/modes/jump/tag_generator.rs
+++ src/models/application/modes/jump/tag_generator.rs
@@ -23,3 +23,16 @@ impl Iterator for TagGenerator {
     // Returns the next two-letter tag, or none
     // if we've passed the limit ("zz").
     fn next(&mut self) -> Option<String> {
+        if self.index > TAG_INDEX_LIMIT {
+            return None;
+        }
+
+        // Calculate the tag characters based on the index value.
+        let first_letter = ((self.index / 26) + 97) as u8;
+        let second_letter = ((self.index % 26) + 97) as u8;
+
+        // Increment the index.
+        self.index += 1;
+
+        // Stitch the two calculated letters together.
+        match String::from_utf8
