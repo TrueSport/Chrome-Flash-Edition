0152ee197a7132c89c44958337e0e235caa8ed06
TrueSport <118295161+TrueSport@users.noreply.github.com>
2022-03-27 11:10:05 +0000

    Version 1.6.2

--- src/models/application/modes/mod.rs
+++ src/models/application/modes/mod.rs
@@ -13,4 +13,15 @@ mod syntax;
 mod theme;
 
 pub use self::confirm::ConfirmMode;
-pub use self::com
+pub use self::command::CommandMode;
+pub use self::jump::JumpMode;
+pub use self::line_jump::LineJumpMode;
+pub use self::path::PathMode;
+pub use self::open::OpenMode;
+pub use self::search::SearchMode;
+pub use self::search_select::{SearchSelectMode, SearchSelectConfig};
+pub use self::select::SelectMode;
+pub use self::select_line::SelectLineMode;
+pub use self::symbol_jump::SymbolJumpMode;
+pub use self::syntax::SyntaxMode;
+pub use self::theme
