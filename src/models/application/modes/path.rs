f1698f774ced1819d99bf8829932943c7bf13ec0
TrueSport <118295161+TrueSport@users.noreply.github.com>
2022-03-28 05:53:50 +0000

    Solución de errores

--- src/models/application/modes/path.rs
+++ src/models/application/modes/path.rs
@@ -8,4 +8,20 @@ pub struct PathMode {
 impl PathMode {
     pub fn new(initial_path: String) -> PathMode {
         PathMode {
-            input: initi
+            input: initial_path,
+            save_on_accept: false
+        }
+    }
+    pub fn push_char(&mut self, c: char) {
+        self.input.push(c);
+    }
+    pub fn pop_char(&mut self) {
+        self.input.pop();
+    }
+}
+
+impl fmt::Display for PathMode {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "PATH")
+    }
+}
