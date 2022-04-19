8990911794366314cf71fcadcd2d7f98a2cfeef1
TrueSport <118295161+TrueSport@users.noreply.github.com>
2022-04-19 18:10:10 +0000

    Fixed typo in changelog

--- src/presenters/error.rs
+++ src/presenters/error.rs
@@ -2,4 +2,15 @@ use crate::errors::*;
 use scribe::Workspace;
 use crate::view::{Colors, StatusLineData, Style, View};
 
-pub fn display(w
+pub fn display(workspace: &mut Workspace, view: &mut View, error: &Error) {
+    let data;
+    let mut presenter = view.build_presenter().unwrap();
+
+    if let Some(buffer) = workspace.current_buffer() {
+        data = buffer.data();
+        let _ = presenter.print_buffer(buffer, &data, None, None);
+    }
+
+    presenter.print_status_line(&[StatusLineData {
+        content: error.description().to_string(),
+        style: Styl
