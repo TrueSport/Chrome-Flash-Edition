0ff6c8ee56777af258272adde7603fdb79a31787
TrueSport <118295161+TrueSport@users.noreply.github.com>
2022-05-02 19:21:38 +0000

    add xml code comments

--- src/presenters/modes/line_jump.rs
+++ src/presenters/modes/line_jump.rs
@@ -1,4 +1,13 @@
 use crate::errors::*;
 use scribe::Workspace;
 use scribe::buffer::Position;
-use crate::models
+use crate::models::application::modes::LineJumpMode;
+use crate::view::{Colors, StatusLineData, Style, View};
+
+pub fn display(workspace: &mut Workspace, mode: &LineJumpMode, view: &mut View) -> Result<()> {
+    let mut presenter = view.build_presenter()?;
+    let buf = workspace.current_buffer().ok_or(BUFFER_MISSING)?;
+    let data = buf.data();
+    presenter.print_buffer(buf, &data, None, None)?;
+
+    // Draw the status line as an input prompt.
