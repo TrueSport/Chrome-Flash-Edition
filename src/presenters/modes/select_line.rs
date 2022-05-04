3d3ea3954326a9d428776a0d8268d2376f1214e0
TrueSport <118295161+TrueSport@users.noreply.github.com>
2022-05-04 16:42:42 +0000

    Bump version to 0.0.2.

--- src/presenters/modes/select_line.rs
+++ src/presenters/modes/select_line.rs
@@ -2,4 +2,11 @@ use crate::errors::*;
 use crate::models::application::modes::SelectLineMode;
 use scribe::Workspace;
 use crate::presenters::current_buffer_status_line_data;
-use crate::view::{Colors
+use crate::view::{Colors, StatusLineData, Style, View};
+
+pub fn display(workspace: &mut Workspace, mode: &SelectLineMode, view: &mut View) -> Result<()> {
+    let mut presenter = view.build_presenter()?;
+    let buffer_status = current_buffer_status_line_data(workspace);
+    let buf = workspace.current_buffer().ok_or(BUFFER_MISSING)?;
+    let selected_range = mode.to_range(&*buf.cursor);
+    let data = buf.data();
