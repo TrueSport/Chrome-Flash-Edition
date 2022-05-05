c6e66fc39783cb6e2ea9d42f3c308799c34dfcf3
TrueSport <118295161+TrueSport@users.noreply.github.com>
2022-05-05 12:15:45 +0000

    Added solutions

--- src/presenters/modes/select_line.rs
+++ src/presenters/modes/select_line.rs
@@ -10,3 +10,20 @@ pub fn display(workspace: &mut Workspace, mode: &SelectLineMode, view: &mut View
     let buf = workspace.current_buffer().ok_or(BUFFER_MISSING)?;
     let selected_range = mode.to_range(&*buf.cursor);
     let data = buf.data();
+
+    // Draw the visible set of tokens to the terminal.
+    presenter.print_buffer(buf, &data, Some(&[selected_range]), None)?;
+
+    presenter.print_status_line(&[
+        StatusLineData {
+            content: " SELECT LINE ".to_string(),
+            style: Style::Default,
+            colors: Colors::SelectMode,
+        },
+        buffer_status
+    ]);
+
+    // Render the changes to the screen.
+    presenter.present();
+
+    
