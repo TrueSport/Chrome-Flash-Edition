476bb5068f8a9a3cfdc7edfdb5b78c2759de8263
TrueSport <118295161+TrueSport@users.noreply.github.com>
2022-10-29 21:48:32 +0000

    block: introduce the BFQ-v7r8 I/O sched for 3.4

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -18,4 +18,15 @@ pub fn inclusive_range(line_range: &LineRange, buffer: &mut Buffer) -> Range {
     let end_position = if line_count > next_line {
         // There's a line below the end of the range, so just use that
         // to capture the last line and its trailing newline character.
-   
+        Position {
+            line: next_line,
+            offset: 0,
+        }
+    } else {
+        // There isn't a line below the end of the range, so try to get
+        // the last line's length and use that as the ending offset.
+        match data.lines().nth(line_range.end()) {
+            Some(line_content) => {
+                // Found the last line's content; use it.
+                Position {
+                    li
