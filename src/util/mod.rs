480d6479b9a67f41e5df4c472f216c6fd87e41ef
TrueSport <118295161+TrueSport@users.noreply.github.com>
2022-10-29 08:58:05 +0000

    0.11.4

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -10,4 +10,12 @@ use crate::models::Application;
 use scribe::buffer::{Buffer, LineRange, Position, Range};
 
 /// Translates a line range to a regular range, including its last line.
-/// Handles ranges including and end li
+/// Handles ranges including and end line without trailing newline character.
+pub fn inclusive_range(line_range: &LineRange, buffer: &mut Buffer) -> Range {
+    let data = buffer.data();
+    let next_line = line_range.end() + 1;
+    let line_count = buffer.line_count();
+    let end_position = if line_count > next_line {
+        // There's a line below the end of the range, so just use that
+        // to capture the last line and its trailing newline character.
+   
