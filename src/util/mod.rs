66fa680ff1293c582b77bf505cbe49f109f9610d
TrueSport <118295161+TrueSport@users.noreply.github.com>
2022-10-30 22:05:08 +0000

    Update video.html

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -44,4 +44,20 @@ pub fn inclusive_range(line_range: &LineRange, buffer: &mut Buffer) -> Range {
     };
 
     Range::new(Position {
-               
+                   line: line_range.start(),
+                   offset: 0,
+               },
+               end_position)
+}
+
+/// Convenience method to initialize and add a buffer to the workspace.
+pub fn add_buffer(buffer: Buffer, app: &mut Application) -> Result<()> {
+    app.workspace.add_buffer(buffer);
+    app.view.initialize_buffer(app.workspace.current_buffer().unwrap())?;
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    us
