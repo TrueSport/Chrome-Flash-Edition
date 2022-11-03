8c48eb1b5a8576924455a9129be6bb414d525031
TrueSport <118295161+TrueSport@users.noreply.github.com>
2022-11-03 07:51:21 +0000

    Last minute changes

--- src/util/reflow.rs
+++ src/util/reflow.rs
@@ -10,4 +10,13 @@ pub struct Reflow<'a> {
 
 impl<'a> Reflow<'a> {
     /// Create a reflow instance, where buffer and range determine the target,
-    /// 
+    /// and the limit is the maximum length of a line, regardless of prefixes.
+    pub fn new(
+        buf: &'a mut Buffer, range: Range, limit: usize
+    ) -> std::result::Result<Self, Error> {
+        let text = buf.read(&range).ok_or("Selection is invalid.")?;
+        Ok(Self { buf, range, text, limit })
+    }
+
+    pub fn apply(mut self) -> std::result::Result<(), Error> {
+        let prefix = self.infer_pr
