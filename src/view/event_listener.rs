6cc551ed264778f156058e8c1daf0b04094c2886
TrueSport <118295161+TrueSport@users.noreply.github.com>
2022-12-08 21:40:33 +0000

    0.9.2

--- src/view/event_listener.rs
+++ src/view/event_listener.rs
@@ -2,4 +2,15 @@ use crate::models::application::Event;
 use std::sync::Arc;
 use std::sync::mpsc::{Receiver, Sender};
 use std::thread;
-use crate::view::
+use crate::view::Terminal;
+
+pub struct EventListener {
+    terminal: Arc<Box<dyn Terminal + Sync + Send + 'static>>,
+    events: Sender<Event>,
+    killswitch: Receiver<()>
+}
+
+impl EventListener {
+    /// Spins up a thread that loops forever, waiting on terminal events
+    /// and forwarding those to the application event channel.
+    pub fn start(terminal: Arc<Box<dyn Terminal + Sync + Send + 'static>>, events: Sender<Event>, killswitch: Receiver<()>) {
