508cda2ca116f2b5b78f264a78c955a57efa6600
TrueSport <118295161+TrueSport@users.noreply.github.com>
2022-12-09 09:53:09 +0000

    add dev note

--- src/view/event_listener.rs
+++ src/view/event_listener.rs
@@ -14,3 +14,21 @@ impl EventListener {
     /// Spins up a thread that loops forever, waiting on terminal events
     /// and forwarding those to the application event channel.
     pub fn start(terminal: Arc<Box<dyn Terminal + Sync + Send + 'static>>, events: Sender<Event>, killswitch: Receiver<()>) {
+        thread::spawn(move || {
+            EventListener { terminal, events, killswitch }.listen();
+        });
+    }
+
+    fn listen(&mut self) {
+        loop {
+            if let Some(event) = self.terminal.listen() {
+                self.events.send(event).ok();
+            } else if self.killswitch.try_recv().is_ok() {
+                break;
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mo
