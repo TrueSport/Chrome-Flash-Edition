2e2ee8e1356d6dbe2bb191e1940993e625c8eab0
TrueSport <118295161+TrueSport@users.noreply.github.com>
2022-12-10 17:08:15 +0000

    Remove debugging logs

--- src/view/mod.rs
+++ src/view/mod.rs
@@ -10,4 +10,18 @@ mod theme_loader;
 // Published API
 pub use self::data::StatusLineData;
 pub use self::buffer::{LexemeMapper, MappedLexeme};
-pu
+pub use self::style::Style;
+pub use self::color::{Colors, RGBColor};
+pub use self::presenter::Presenter;
+pub use self::terminal::*;
+
+use crate::errors::*;
+use crate::input::Key;
+use crate::models::application::{Event, Preferences};
+use self::buffer::{RenderCache, RenderState};
+use self::buffer::ScrollableRegion;
+use self::event_listener::EventListener;
+use scribe::buffer::Buffer;
+use std::cmp;
+use std::collections::HashMap;
+use std::rc::
