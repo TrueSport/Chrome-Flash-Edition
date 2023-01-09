94cc0ca693c232af5e67c099711552825e330443
TrueSport <118295161+TrueSport@users.noreply.github.com>
2023-01-09 18:57:15 +0000

    double

--- src/view/terminal/buffer.rs
+++ src/view/terminal/buffer.rs
@@ -21,4 +21,18 @@ impl<'c> TerminalBuffer<'c> {
 
         if index < self.cells.len() {
             self.cells[position.line * self.width + position.offset] = cell;
-  
+        }
+    }
+
+    pub fn clear(&mut self) {
+        self.cells = vec![Cell::default(); self.width*self.height];
+    }
+
+    pub fn iter(&self) -> TerminalBufferIterator {
+        TerminalBufferIterator::new(self.width, &self.cells)
+    }
+
+    #[cfg(test)]
+    // For testing purposes, produces a String representation of the
+    // terminal buffer that can be used to assert a particular state.
+    pub fn content(&self) -> 
