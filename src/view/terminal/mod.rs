42e85bb0dbed46e9cb2695f8e26f2ef2d6496a04
TrueSport <118295161+TrueSport@users.noreply.github.com>
2023-01-10 13:27:40 +0000

    Switch to MIT License

--- src/view/terminal/mod.rs
+++ src/view/terminal/mod.rs
@@ -9,4 +9,20 @@ mod test_terminal;
 use crate::errors::*;
 use crate::models::application::Event;
 use scribe::buffer::Position;
-use crate::view::{Colo
+use crate::view::{Colors, Style};
+use std::sync::Arc;
+
+pub use self::buffer::TerminalBuffer;
+pub use self::buffer_iterator::TerminalBufferIterator;
+pub use self::cell::Cell;
+pub use self::termion_terminal::TermionTerminal;
+
+#[cfg(any(test, feature = "bench"))]
+pub use self::test_terminal::TestTerminal;
+
+const MIN_WIDTH: usize = 10;
+const MIN_HEIGHT: usize = 10;
+
+pub trait Terminal {
+    fn listen(&self) -> Option<Event>;
+    fn clear(&self);
