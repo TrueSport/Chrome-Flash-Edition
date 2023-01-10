3ee0dd2485e6a833478e44060d4b2af7dc45000c
TrueSport <118295161+TrueSport@users.noreply.github.com>
2023-01-10 22:05:36 +0000

    Added missing semicolons.

--- src/view/terminal/termion_terminal.rs
+++ src/view/terminal/termion_terminal.rs
@@ -6,4 +6,13 @@ use mio::{Events, Poll, PollOpt, Ready, Token};
 use mio::unix::EventedFd;
 use super::Terminal;
 use std::io::Stdout;
-use std::os::unix::io::
+use std::os::unix::io::AsRawFd;
+use scribe::buffer::{Distance, Position};
+use self::termion::color::{Bg, Fg};
+use self::termion::{color, cursor};
+use self::termion::input::{Keys, TermRead};
+use self::termion::raw::{IntoRawMode, RawTerminal};
+use self::termion::screen::{AlternateScreen, IntoAlternateScreen};
+use self::termion::style;
+use std::io::{BufWriter, Stdin, stdin, stdout, Write};
+use std::fmt::Di
