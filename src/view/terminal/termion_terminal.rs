57b37322caa334fbcecdaf3509346ac867127cb7
TrueSport <118295161+TrueSport@users.noreply.github.com>
2023-01-12 07:41:07 +0000

    Update appveyor config

--- src/view/terminal/termion_terminal.rs
+++ src/view/terminal/termion_terminal.rs
@@ -15,4 +15,21 @@ use self::termion::raw::{IntoRawMode, RawTerminal};
 use self::termion::screen::{AlternateScreen, IntoAlternateScreen};
 use self::termion::style;
 use std::io::{BufWriter, Stdin, stdin, stdout, Write};
-use std::fmt::Di
+use std::fmt::Display;
+use std::ops::Drop;
+use std::sync::Mutex;
+use std::time::Duration;
+use crate::view::{Colors, Style};
+use unicode_segmentation::UnicodeSegmentation;
+use signal_hook::iterator::Signals;
+
+use self::termion::event::Key as TermionKey;
+use crate::input::Key;
+use crate::models::application::Event;
+
+const STDIN_INPUT: Token = Token(0);
+const RESIZE: Token = Token(1);
+
+pub struct TermionTerminal {
+    event_listener: Poll,
+    s
